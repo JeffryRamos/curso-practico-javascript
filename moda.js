09271b9e7f79185fe3eeb1f8819e0c13edd7bf23
JeffryRamos <jefryfranciscoramos@gmail.com>
2022-04-11 14:36:27 -0600

    desarrollo de la moda

---
+++ moda.js
@@ -0,0 +1,29 @@
+const lista1 = [
+    1,2,3,1,2,3,5,1,2,3,1
+];
+
+const lista1Count = {};
+
+lista1.map(
+    function(elemento){
+        if (lista1Count[elemento]) {
+            lista1Count[elemento] += 1;
+        }else{
+            lista1Count[elemento]= 1;
+        }
+        
+    }
+);
+/*
+const lista1Array = Object.entries(lista1Count).sort(
+    function(valorAcumulado, nuevoValor){
+        valorAcumulado - nuevoValor
+    }
+);
+*/
+const lista1Array = Object.entries(lista1Count).sort(
+    function (elementoA, elementoB){
+        return elementoA[1] -elementoB[1];
+    }
+);
+const moda = lista1Array[lista1Array.length - 1];
